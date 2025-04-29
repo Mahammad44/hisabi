f1b3b8d89815d1733b0b68ab5dd73ba608441d82
mahammad44 <mahammadbincarlton@gmail.com>
2025-04-30 00:45:25 +0100

    basic backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on http://{}", addr);
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn root() -> &'static str {
+    "Welcome to Hisabi Backend"
+}
+
